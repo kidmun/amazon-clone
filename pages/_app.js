a341637ce1a298cfedeb1758969651a195748917
kidmun <kidusguade41@gmail.com>
2022-11-15 01:34:53 -0800

    add shipping page

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,37 @@
 import '../styles/globals.css';
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
 import { StoreProvider } from '../utils/Store';
+import { useRouter } from 'next/router';
+function Auth({ children }) {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push('/unauthorized?message=login required');
+    },
+  });
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  return children;
+}
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <StoreProvider>
+    <StoreProvider>
+      {Component.auth ? (
+        <Auth>
+          <Component {...pageProps} />
+        </Auth>
+      ) : (
         <Component {...pageProps} />
-      </StoreProvider>
-    </SessionProvider>
+      )}
+    </StoreProvider>
+  </SessionProvider>
   );
 }
+
 
 export default MyApp;
--- utils/Store.js
+++ utils/Store.js
@@ -5,7 +5,9 @@ export const Store = createContext();
 
 const initialState = {
   cart: Cookies.get('cart') ? JSON.parse(Cookies.get('cart')):{
-    cartItems: [],
+    cartItems: [], shippingAddress: {
+      
+    }
   },
 };
 
@@ -61,6 +63,17 @@ function reducer(state, action) {
           cartItems: [],
           shippingAddress: { location: {} },
           paymentMethod: '',
+        },
+      };
+      case 'SAVE_SHIPPING_ADDRESS': 
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: {
+            ...state.cart.shippingAddress,
+            ...action.payload,
+          },
         },
       };
 
